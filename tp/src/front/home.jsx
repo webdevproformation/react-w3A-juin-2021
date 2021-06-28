c639ac979dbed811e2bcb5a1d528c6316a9a7f0a
Malik H <malik.h@webdevpro.net>
2021-06-28 12:08:23 +0200

    rappel sur les boucles

--- tp/src/front/home.jsx
+++ tp/src/front/home.jsx
@@ -1,3 +1,38 @@
-const Home = () =>  <h1>je suis la home page</h1>
-
+const Home = () => { 
+    const articles = [
+        {
+            id: 1 ,
+            titre : "article 1",
+            contenu : "lorem ipsum "
+        },
+        {
+            id: 2 ,
+            titre : "article 2",
+            contenu : "lorem ipsum "
+        },
+        {
+            id: 3 ,
+            titre : "article 3",
+            contenu : "lorem ipsum "
+        }
+    ];
+    return (
+        <>
+            <h1>je suis la home page</h1>
+            {articles.map((article, index) => 
+                <article className="col-4" key={index}>
+                    <div className="card">
+                        <header class="card-header">
+                            <h2>{article.titre}</h2>
+                        </header>
+                        <img src="http://via.placeholder.com/300x200" alt="" />
+                        <div className="card-body">
+                            {article.contenu}
+                        </div>
+                    </div>
+                </article>
+            )}
+        </>
+    )
+}  
 export default Home;
