fefbc929de25d3055c010b31058c42c940d23114
Malik H <malik.h@webdevpro.net>
2021-06-28 11:04:04 +0200

    start projet tp

---
+++ tp/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="row">
+        <h1 className="text-success">Projet semaine 3</h1>  
+      </header>  
+    </div>
+  );
+}
+
+export default App;
