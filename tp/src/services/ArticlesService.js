4b34aef0736ebac164721f55abb0a0425d54b295
Malik H <malik.h@webdevpro.net>
2021-06-28 12:56:55 +0200

    cas pratique filter

--- tp/src/services/ArticlesService.js
+++ tp/src/services/ArticlesService.js
@@ -3,17 +3,23 @@ class ArticlesService{
         {
             id: 1 ,
             titre : "article 1",
-            contenu : "lorem ipsum "
+            contenu : "lorem ipsum ",
+            like : 2,
+            isActif : true 
         },
         {
             id: 2 ,
             titre : "article 2",
-            contenu : "lorem ipsum "
+            contenu : "lorem ipsum ",
+            like : 0 ,
+            isActif : false 
         },
         {
             id: 3 ,
             titre : "article 3",
-            contenu : "lorem ipsum "
+            contenu : "lorem ipsum ",
+            like : 10,
+            isActif : true
         }
     ];
     getData(){
