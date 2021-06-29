b6a56b03ea8a90c892e87f125d1301706b482445
Malik H <malik.h@webdevpro.net>
2021-06-29 10:26:18 +0200

    page d'accueil du back office

--- tp/src/back/dashboard.jsx
+++ tp/src/back/dashboard.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import ArticlesService from "../services/ArticlesService";
 
 class Dashboard extends Component {
-    state = {  }
+    state = { 
+        articles : (new ArticlesService()).getData()
+    }
     render() { 
+        const {articles} = this.state;
         return ( 
-            <h1>Bienvenu dans le back office </h1>
+            <>
+                <div className="col-12">
+                    <h1>Bienvenu dans le back office </h1>
+                </div>
+                <div className="col-12">
+                <table className="table table-striped table-sm">
+                    <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>titre</th>
+                        <th>status</th>
+                        <th>actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {articles.map( ( article , index ) => {
+                        return <tr key={index}>
+                                <td>{article.id}</td>
+                                <td>{article.titre}</td>
+                                <td>{  article.isActif ? "Actif" : "Inactif" }</td>
+                                <td>
+                                    <button className="btn btn-primary">voir</button>
+                                    <button className="btn btn-warning mx-3">modifier</button>
+                                    <button className="btn btn-danger">supprimer</button>
+                                </td>
+                            </tr>
+                    } )}
+                    </tbody>
+                </table>
+                </div>
+            </>
         );
     }
 }
